33af6b30b7c817426811e193c0b7c21350f08dd4
Caitlin Harrington <cnharrington97@gmail.com>
2018-11-13 14:42:02 -0500

    1 and 2 finished

--- biocomputing10.py
+++ biocomputing10.py
@@ -25,11 +25,11 @@ def nllikequadratic(p,obs):
     nllquadratic=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nllquadratic
 
-    #estimate parameters by minimizing neg log likelihood
-    initialGuessquadratic=numpy.array([1,1,1,1])
-    fitquadratic=minimize(nllikequadratic,initialGuessquadratic,method="Nelder-Mead",options={'disp':True},args=data)
-    
-    print (fitquadratic.x)
+#estimate parameters by minimizing neg log likelihood
+initialGuessquadratic=numpy.array([1,1,1,1])
+fitquadratic=minimize(nllikequadratic,initialGuessquadratic,method="Nelder-Mead",options={'disp':True},args=data)
+
+print (fitquadratic.x)
     
 #def custom likelihood function for linear
 def nllikelinear(p,obs):
@@ -41,48 +41,58 @@ def nllikelinear(p,obs):
     nlllinear=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nlllinear
 
-    #estimate parameters by minimizing neg log likelihood
-    initialGuesslinear=numpy.array([1,1,1])
-    fitlinear=minimize(nllikelinear,initialGuesslinear,method="Nelder-Mead",options={'disp':True},args=data)
+#estimate parameters by minimizing neg log likelihood
+initialGuesslinear=numpy.array([1,1,1])
+fitlinear=minimize(nllikelinear,initialGuesslinear,method="Nelder-Mead",options={'disp':True},args=data)
     
-    print (fitlinear.x)
+print (fitlinear.x)
 
 #which model is more appropriate
 from scipy import stats
-teststat=2.0*(fitquadratic.fun-fitlinear.fun)
-df=(len(fitquadratic.x)-len(fitlinear.x))/1.0
+teststat=2.0*(fitlinear.fun-fitquadratic.fun)
+df=(len(fitquadratic.x)-len(fitlinear.x))
 1-stats.chi2.cdf(teststat,df)
+
+#since the p value is greater than 0.05 there is no significant difference between the two models, so you can use the simple linear model 
+
 
 #2
 import scipy
 import scipy.integrate as spint
 
-def ddSim (y,t0,r,a):
+def ddSim (y,t0,r1,r2,a11,a12,a21,a22):
     N1=y[0]
     N2=y[1]
-    R1=r[0]
-    R2=r[1]
-    a11=a[0]
-    a12=a[1]
-    a21=a[2]
-    a22=a[3]
-    dN1dt=R1*(1-N1*a11-N2*a12)*N1
-    dN2dt=R2*(1-N2*a22-N1*a21)*N2
+    dN1dt=r1*(1-N1*a11-N2*a12)*N1
+    dN2dt=r2*(1-N2*a22-N1*a21)*N2
     
     return [dN1dt,dN2dt]
 
 #model simulation 1 a12<a11, a21<a22
-params=([.05,.05],[.01,.005,.003,.02])
-N0=[0.01,0.01]
-times=range(0,600)
+params=(.5,.5,.01,.005,.003,.02)
+N0=[1,1]
+
+times=range(0,100)
 
 modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
-modelOutput=pandas.DataFrame({"t":times, "N1": modelSim2[:,0], "N2": modelSim2[:,1]})
+modelOutput=pandas.DataFrame({"t":times, "N1": modelSim[:,0], "N2": modelSim[:,1]})
 ggplot(modelOutput)+geom_line(aes(x="t", y="N1"), color="red")+geom_line(aes(x="t", y="N2"), color="blue")
 
 #model simulation 2 a12>a11, a21<a22
-params=([.05,.05],[.1,3,.003,.02])
-N0=[0.01,0.01]
-times=range(0,600)
+params2=(.5,.5,.01,.015,.003,.01)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params2)
+modelOutput=pandas.DataFrame({"t":times, "N1": modelSim[:,0], "N2": modelSim[:,1]})
+ggplot(modelOutput)+geom_line(aes(x="t", y="N1"), color="red")+geom_line(aes(x="t", y="N2"), color="blue")
 
-    
+
+#model simulation 3 a12<a11, a22<a21
+params3=(.5,.5,.01,.005,.02,.003)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params3)
+modelOutput=pandas.DataFrame({"t":times, "N1": modelSim[:,0], "N2": modelSim[:,1]})
+ggplot(modelOutput)+geom_line(aes(x="t", y="N1"), color="red")+geom_line(aes(x="t", y="N2"), color="blue")
+
+#model simulation 4 a11<a12 and a22<a21
+params4=(.5,.5,.01,.015,.02,.003)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params4)
+modelOutput=pandas.DataFrame({"t":times, "N1": modelSim[:,0], "N2": modelSim[:,1]})
+ggplot(modelOutput)+geom_line(aes(x="t", y="N1"), color="red")+geom_line(aes(x="t", y="N2"), color="blue")
